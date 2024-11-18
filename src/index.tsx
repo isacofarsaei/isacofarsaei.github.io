c8f1b283aa8dfa461b0ba53b93bab8c1e3c2bf0b
isacofarsaei <isacofarsaei@gmail.com>
2024-11-18 21:32:17 +0330

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './Utils/Fonts/DTHULUTH.TTF'; 
+import './Utils/Fonts/DTNASKH0.TTF';
+import './Utils/Fonts/DTNASKH1.TTF';
+import './Utils/Fonts/DTNASKH2.TTF';
+import './Utils/Fonts/DTNASKH3.TTF';
+import './Utils/Fonts/DTNASKH4.TTF';
+import './Utils/Fonts/IRANSans1.ttf';
+import './Utils/Fonts/IRANSans3.ttf';
+import './Utils/Fonts/IRANSans4.ttf';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
