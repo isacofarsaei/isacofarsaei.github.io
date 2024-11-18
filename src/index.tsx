1716e333832c0503700e129c3118db96ebd6114a
isacofarsaei <isacofarsaei@gmail.com>
2024-11-18 22:24:18 +0330

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,14 +12,17 @@ import './Utils/Fonts/DTNASKH4.TTF';
 import './Utils/Fonts/IRANSans1.ttf';
 import './Utils/Fonts/IRANSans3.ttf';
 import './Utils/Fonts/IRANSans4.ttf';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
